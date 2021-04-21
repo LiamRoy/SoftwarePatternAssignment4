7adc158bb5dd240b42c4be008afefb9897ae4f27
liamr <liamr@192.168.1.93>
2021-04-21 16:03:17 +0100

    Added Strat Pattern to User Service

---
+++ CA4LiamRoy-copy/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.demo.entities.User;
+import com.example.demo.web.dto.UserRegistrationDto;
+
+//Strategy Pattern
+public interface UserService {
+
+	User save(UserRegistrationDto registrationDto);
+	public User findByUsernameAndPassword(String username, String password);
+    public User findByUsername(String username);
+  
+	
+}
