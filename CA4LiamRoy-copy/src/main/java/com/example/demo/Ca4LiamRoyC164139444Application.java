7cf464a2cc7125b1edd11c828d6bad19029eeec9
liamr <liamr@192.168.1.93>
2021-04-21 15:59:41 +0100

    First Commit

---
+++ CA4LiamRoy-copy/src/main/java/com/example/demo/Ca4LiamRoyC164139444Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ca4LiamRoyC164139444Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ca4LiamRoyC164139444Application.class, args);
+	}
+
+}
---
+++ CA4LiamRoy-copy/src/test/java/com/example/demo/Ca4LiamRoyC164139444ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ca4LiamRoyC164139444ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
